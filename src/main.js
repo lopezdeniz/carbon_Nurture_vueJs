1a5ed772982561d92a3845a474e59eafbfeea770
mrLopez <lopesdenis@mail.ru>
2024-12-05 20:31:19 -0500

    card fix

--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router"; // Подключение маршрутизатора
+import "aos/dist/aos.css"; // Подключение стилей AOS
+import AOS from "aos"; // Импорт AOS
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+
+// Инициализация AOS при монтировании приложения
+app.mixin({
+  mounted() {
+    AOS.init({
+      duration: 800, // Длительность анимации в миллисекундах
+      easing: "ease-in-out", // Тип анимации
+      once: true, // Запуск анимации только один раз
+      mirror: false, // Не запускать анимацию при прокрутке назад
+    });
+  },
+});
+
+app.use(router).mount("#app");
